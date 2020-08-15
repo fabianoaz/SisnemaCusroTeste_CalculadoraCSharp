98c5b2894f2c31b595eea849c6a954e84888a7fc
fabianoaz <fabianoaz@hotmail.com>
2020-08-15 18:06:56 -0300

    Calculadora simples com testes unitários em C#

---
+++ CalculadoraSimples_NUnit/App/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraSimples_NUnit.App
+{
+    public class Calculadora
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtrai(int a, int b)
+        {
+            return a - b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public int Multiplica(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ CalculadoraSimples_NUnit/Test/Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CalculadoraSimples_NUnit.App;
+
+namespace CalculadoraSimples_NUnit.Test
+{
+    public class Test_Calc
+    {
+        
+
+        [Test]
+        public static void SomaValoresPositivos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(15, calc.Soma(10,5));
+        }
+        [Test]
+        public static void SomaValoresNegativos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(-15, calc.Soma(-10, -5));
+        }
+
+        [Test]
+        public static void SubtraiValoresPositivos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(5, calc.Subtrai(10,5));
+        }
+        [Test]
+        public static void SubtraiValoresNegativos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(-5, calc.Subtrai(-10, -5));
+        }
+
+        [Test]
+        public static void DivideValoresPositivos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(2, calc.Divide(10,5));
+        }
+        [Test]
+        public static void DivideValoresNegativos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(2, calc.Divide(-10, -5));
+        }
+
+        [Test]
+        public static void MultiplicaValoresPositivos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(50, calc.Multiplica(10,5));
+        }
+        [Test]
+        public static void MultiplicaValoresNegativos()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.AreEqual(50, calc.Multiplica(-10, -5));
+        }
+
+    }
+}
